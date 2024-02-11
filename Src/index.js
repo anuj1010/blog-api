a48d0c7faf7598037fd49708eb03b3989941c4b3
Anuj Singh <editingwithanuj@gmail.com>
2024-02-11 22:20:25 +0530

    cors fix

--- Src/index.js
+++ Src/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: "https://anuj-blog-insight.netlify.app/" })); // Allow all origins
+app.use(cors({ origin: "https://anuj-blog-insight.netlify.app" })); // Allow all origins
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/blog", router);
